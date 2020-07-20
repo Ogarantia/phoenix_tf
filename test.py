7c8d1826e302f96d615afbd1ad29f1e54e25c47a
Sebastien Iooss <sebastien.iooss@upstride.io>
2020-07-20 18:30:08 +0200

    2 spaces indent python code

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import unittest
 import tensorflow as tf
 import numpy as np
-from modules.upstride_tf   import generic_layers
+from modules.upstride_tf import generic_layers
 from tests.test import *
 from src.python.upstride.type2.tf.keras.test_layers import TestType2LayersTF2Upstride
 
 if __name__ == "__main__":
-    unittest.main()
+  unittest.main()
