2b35bfd2a1109668d376dad629c6825be51a7a29
youelebr <youenn.lebras@upstride.io>
2020-07-23 10:55:02 +0200

    Setup test

---
+++ tests/cpp_tests/tests.cpp
@@ -0,0 +1,48 @@
+/**
+                                                    Report
+
+    The following code contains the unit tests for the c++ part of the phoenix engine, it performs
+    basic operations verifications and verifies the maths properties for each operator.
+
+    It contains:
+
+
+**/
+
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include <iostream>
+#include <fstream>
+#include <random>
+#include <limits>
+
+#include "doctest/doctest.h"
+#include "utiles.hpp"
+
+
+//Constants
+
+/* =============================================================================
+                                 PHOENIX 
+============================================================================= */
+
+
+TEST_CASE("Test:Utiles")
+{
+  std::cout << "---- Test: Utiles functions" << std::endl;
+
+  SUBCASE(" Test: Utiles::computeConvOutputSize")
+  {
+    std::cout << " Test: Utiles::computeConvOutputSize" << std::endl;
+
+
+
+    std::cout << " COUCOU       : " << std::endl;
+    std::cout<< std::endl;
+
+    // CHECK((expectedOut1 == a + d));
+    std::cout << std::endl;
+  }
+
+}
