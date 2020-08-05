b56a58e3aed7640d38170b78ce44766b49d47cef
youelebr <youenn.lebras@upstride.io>
2020-08-05 11:28:55 +0200

    [Tests] Update tests; TF doesn't support backward propagation with dilations larger than 1 so we cannot compare but it return no error for us. [Conv2DBwd] update the register operations conv2DGrad by adding dilations as parameter

--- tests/python_tests/test_onednn.py
+++ tests/python_tests/test_onednn.py
@@ -42,11 +42,8 @@ class TestConv2D(unittest.TestCase):
         self.run_conv2d_test(img_size=112, filter_size=3, in_channels=32, out_channels=48, padding='SAME', strides=[1, 2], dilations=[3, 4])
 
 class TestConv2DGrad(unittest.TestCase):
-    def run_conv2dgrad_test(self, img_size=128, filter_size=3, in_channels=2, out_channels=1, padding='SAME', strides=[1,1], dilations=[1,1], pytorch=0):
-      if pytorch == 1:
-        import torch
-        from torch import nn
-      
+    def run_conv2dgrad_test(self, img_size=128, filter_size=3, in_channels=2, out_channels=1, padding='SAME', strides=[1,1], dilations=[1,1]):
+      """ Runs a single convolution forward and backward and compares the result with TensorFlow output """
       input = tf.random.uniform((1, in_channels, img_size, img_size), dtype=tf.float32)
       filter = tf.random.uniform((out_channels, in_channels, filter_size, filter_size), dtype=tf.float32)
 
@@ -59,20 +56,7 @@ class TestConv2DGrad(unittest.TestCase):
                   padding=padding,
                   dilations=dilations,
                   data_format='NCHW')
-        output_test_summed = tf.reduce_sum(output_test)
-      grad_test = gt.gradient(output_test_summed, filter)
-
-      ## PYTORCH
-      if pytorch == 1:
-        filter_pt = torch.from_numpy(filter)
-        input_pt = torch.from_numpy(input)
-        input_pt.requires_grad = True
-        model_pt = nn.Conv2d(in_channels, out_channels, filter_size, bias=False, padding=1, stride=strides, dilation=dilations)
-        model_pt.weight = nn.Parameter(filter_pt)
-        output_ref = model_pt(input_pt)
-        (output_ref.sum()).backward()
-        grad_reference_input = input_pt.grad
-        grad_reference_PT = model_pt.weight.grad
+      grad_test = gt.gradient(output_test, filter)
 
       ## TENSORFLOW
       # #                            N  H  W  C
@@ -96,22 +80,10 @@ class TestConv2DGrad(unittest.TestCase):
       grad_reference_TF = gt.gradient(output_ref_TF, filter_t)
       #                                              O  I  H  W 
       grad_reference_TF = tf.transpose(grad_reference_TF, [3, 2, 0, 1])
-      output_ref_TF = tf.transpose(output_ref_TF, [0, 3, 1, 2])
 
       ## COMPARISONS
-      # print("conv_Test\n", output_test, "\n")
-      # print("conv_Ref\n", output_ref_TF, "\n")
-      # print("conv_Ref - conv_Test\n", output_ref_TF.numpy() - output_test.numpy(), "\n")
-
-      # print("Grad_Test\n", grad_test, "\n")
-      # print("Grad_Ref_TF\n", grad_reference_TF, "\n")
-      # print("Grad_Ref_TF - Grad_test\n", grad_reference_TF.numpy() - grad_test.numpy(), "\n")
-      
-      # if pytorch == 1:
-      #   print("Grad_Ref_PT - Grad_Ref_TF\n", grad_reference_PT.detach().numpy() - grad_reference_TF.numpy(), "\n")
-      
-      err = tf.math.reduce_max(tf.math.abs(output_test - output_ref_TF))
-      self.assertLess(err, 1e-4, f"Absolute difference with the reference is too big: {err}")
+      err = tf.math.reduce_max(tf.math.abs(grad_test - grad_reference_TF))
+      self.assertLess(err, 1e-2, f"Absolute difference with the reference is too big: {err}")
       print('[Conv2DBwd] Absolute difference:', err.numpy())
 
     def test_conv2dgrad(self):
@@ -121,6 +93,3 @@ class TestConv2DGrad(unittest.TestCase):
       self.run_conv2dgrad_test(img_size=224, filter_size=3, in_channels=3, out_channels=64, padding='VALID')
       self.run_conv2dgrad_test(img_size=224, filter_size=4, in_channels=3, out_channels=64, padding='SAME')
       self.run_conv2dgrad_test(img_size=224, filter_size=5, in_channels=3, out_channels=16, strides=[2, 2])
-      #Doesn't support dilation above 1
-      # self.run_conv2dgrad_test(img_size=112, filter_size=6, in_channels=16, out_channels=32, dilations=[2, 2])
-      # self.run_conv2dgrad_test(img_size=112, filter_size=3, in_channels=32, out_channels=48, padding='SAME', strides=[1, 2], dilations=[3, 4])
