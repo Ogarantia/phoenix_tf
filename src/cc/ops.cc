aaf645990d2f474aae6ff8d4f3a80d57165d45d4
Maxim Karpushin <maxim.karpushin@upstride.io>
2020-08-24 10:48:05 +0200

    PE-151: fixing require_input_grad = True for tests

--- src/cc/ops.cc
+++ src/cc/ops.cc
@@ -127,7 +127,7 @@ REGISTER_OP("UpstrideConv2DGrad")
 #endif
     .Attr(tensorflow::GetConvnetDataFormatAttrString())
     .Attr("groups: int = 1")
-    .Attr("require_input_grad: bool = false")
+    .Attr("require_input_grad: bool = true")
     .SetShapeFn([](tensorflow::shape_inference::InferenceContext* c) {
         c->set_output(0, c->input(1));
         c->set_output(1, c->input(2));
