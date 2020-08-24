31055c71fa46eaed19565e17ca0a8247a7de1bcc
Sebastien Iooss <archimist.linux@gmail.com>
2020-08-24 11:20:59 +0200

    clean script train_simple_network

--- tests/python_high_level_tests/train_simple_network.py
+++ tests/python_high_level_tests/train_simple_network.py
@@ -1,14 +1,15 @@
 import tensorflow as tf
-# import tensorflow_datasets as tfds
 import argparse
 import sys
 
-sys.path.append('src/python')
+# tf.debugging.experimental.enable_dump_debug_info("/tmp/log", tensor_debug_mode="FULL_HEALTH", circular_buffer_size=-1)
+
+sys.path.append('../../src/python')
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--upstride', "-u", type=int, default=1, help='')
 args = parser.parse_args()
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()  # tfds.load("cifar10")
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 x_train = tf.transpose(x_train, [0, 3, 1, 2])
 x_test = tf.transpose(x_test, [0, 3, 1, 2])
@@ -41,12 +42,13 @@ model.compile(optimizer=tf.keras.optimizers.Adam(lr=0.0001),
               metrics=['accuracy'])
 
 
-tensorboard_cb = tf.keras.callbacks.TensorBoard(log_dir='/tmp/log', histogram_freq=1, write_graph=False, write_images=False, profile_batch=0)
+tensorboard_cb = tf.keras.callbacks.TensorBoard(log_dir='/tmp/log', histogram_freq=1, profile_batch=[2, 5], write_graph=False, write_images=False)
 
 
 model.fit(x_train, y_train,
-          epochs=30,
+          epochs=60,
           batch_size=1000,
+          steps_per_epoch=30,
           validation_data=(x_test, y_test),
           callbacks=[tensorboard_cb,
                      tf.keras.callbacks.TerminateOnNaN()
