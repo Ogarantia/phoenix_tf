60c4a1506c92c3f005aec799279b007f355c0c6c
Philipe Moura <philipe.moura@upstride.io>
2020-08-24 10:36:21 +0200

    Introduced unit tests for input grad.

--- tests/python_unittests/test_conv2d.py
+++ tests/python_unittests/test_conv2d.py
@@ -47,15 +47,17 @@ class TestConv2DGrad(unittest.TestCase):
       filter = tf.random.uniform((out_channels, in_channels, filter_size, filter_size), dtype=tf.float32, minval=-0.5, maxval=0.5)
 
       ## UPSTRIDE
-      with tf.GradientTape() as gt:
+      with tf.GradientTape(persistent=True) as gt:
         gt.watch(filter)
+        gt.watch(input)
         output_test = upstride_ops.upstride_conv2d(
                   input, filter,
                   strides=strides,
                   padding=padding,
                   dilations=dilations,
                   data_format='NCHW')
-      grad_test = gt.gradient(output_test, filter)
+      grad_test_filter = gt.gradient(output_test, filter)
+      grad_test_input = gt.gradient(output_test, input)
 
       ## TENSORFLOW
       # #                            N  H  W  C
@@ -66,22 +68,30 @@ class TestConv2DGrad(unittest.TestCase):
       filter_t = tf.identity(filter_t)
       with tf.GradientTape(persistent=True) as gt:
         gt.watch(filter_t)
+        gt.watch(input_t)
         output_ref_TF = tf.nn.conv2d(
                   input_t, filter_t,
                   strides=strides,
                   padding=padding,
                   dilations=dilations) 
 
-      grad_reference_TF = gt.gradient(output_ref_TF, filter_t)
-      #                                              O  I  H  W 
-      grad_reference_TF = tf.transpose(grad_reference_TF, [3, 2, 0, 1])
+      grad_reference_filter_TF = gt.gradient(output_ref_TF, filter_t)
+      grad_reference_input_TF = gt.gradient(output_ref_TF, input_t)
+      #                                                                  O  I  H  W 
+      grad_reference_filter_TF = tf.transpose(grad_reference_filter_TF, [3, 2, 0, 1])
+      grad_reference_input_TF = tf.transpose(grad_reference_input_TF, [0, 3, 1, 2])
 
       ## COMPARISONS
-      err = tf.math.reduce_max(tf.math.abs(grad_test - grad_reference_TF))
-      self.assertLess(err, 1e-4, f"Absolute difference with the reference is too big: {err}")
-      print('[Conv2DBwd] Absolute difference:', err.numpy())
+      err_filter = tf.math.reduce_max(tf.math.abs(grad_test_filter - grad_reference_filter_TF))
+      self.assertLess(err_filter, 1e-4, f"Absolute filter difference compared to the reference is too big: {err_filter}")
+      print('[Conv2DBwd] Absolute filter difference:', err_filter.numpy())
+
+      err_input = tf.math.reduce_max(tf.math.abs(grad_test_input - grad_reference_input_TF))
+      self.assertLess(err_input, 1e-4, f"Absolute input difference compared to the reference is too big: {err_input}")
+      print('[Conv2DBwd] Absolute input difference:', err_input.numpy())
 
     def test_conv2dgrad(self):
+      self.run_conv2dgrad_test(img_size=8, filter_size=3, in_channels=2, out_channels=2, padding='VALID')
       self.run_conv2dgrad_test(img_size=9, filter_size=3, in_channels=3, out_channels=16, padding='VALID')
       self.run_conv2dgrad_test(img_size=9, filter_size=3, in_channels=3, out_channels=16, padding='SAME')
       self.run_conv2dgrad_test(img_size=9, filter_size=3, in_channels=3, out_channels=16, strides=[2, 2])
