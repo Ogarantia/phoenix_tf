e5240e3e4140085a45431e7e120ac88b7aba59fe
Maxim Karpushin <maxim.karpushin@upstride.io>
2020-09-04 12:36:55 +0000

    Merged in bugfix/high_level_tests (pull request #20)
    [Tests] Bugfix to make things work on GTX1650
    
    * [Tests] Consistent file naming
    
    * [Tests] Bugfix to make things work on GTX1650
    
    Approved-by: Philipe Moura

--- tests/python_high_level_tests/train_simple_network_quaternions.py
+++ tests/python_high_level_tests/train_simple_network_quaternions.py
@@ -7,18 +7,29 @@ import sys
 sys.path.append('../../src/python')
 
 
-def Model(framework, factor, input_shape=(3, 32, 32), nclasses=10):
+def Model(upstride=False, input_shape=(3, 32, 32), nclasses=10):
   inputs = tf.keras.layers.Input(input_shape)
   x = inputs
 
-  if hasattr(framework, 'TF2Upstride'):
+  if upstride:
+    import upstride.type2.tf.keras.layers as up
+    framework = up
+    factor = 4
+    print("Using upstride")
+  else:
+    framework = tf.keras.layers
+    factor = 1
+    print("Using Tensorflow")
+
+  if upstride:
     x = framework.TF2Upstride()(x)
 
-  x = framework.Conv2D(4 // factor, 3, data_format='channels_first', use_bias=False, activation=tf.nn.relu, require_input_grad=False)(x)
+  kwargs = {'require_input_grad': False} if upstride else {}
+  x = framework.Conv2D(4 // factor, 3, data_format='channels_first', use_bias=False, activation=tf.nn.relu, **kwargs)(x)
   x = framework.Conv2D(8 // factor, 3, data_format='channels_first', use_bias=False, activation=tf.nn.relu)(x)
   x = framework.Conv2D(16 // factor, 3, data_format='channels_first', use_bias=False, activation=tf.nn.relu)(x)
 
-  if hasattr(framework, 'Upstride2TF'):
+  if upstride:
     x = framework.Upstride2TF()(x)
 
   x = tf.keras.layers.Flatten()(x)
@@ -30,31 +41,29 @@ def Model(framework, factor, input_shape=(3, 32, 32), nclasses=10):
 
 
 def main():
+  gpus = tf.config.experimental.list_physical_devices('GPU')
+  for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
   parser = argparse.ArgumentParser()
   parser.add_argument('--upstride', "-u", type=int, default=1, help='')
   args = parser.parse_args()
+
+  # prepare CIFAR10
   (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
-  x_train, x_test = x_train / 255.0, x_test / 255.0
+  x_train, x_test = tf.cast(x_train, tf.float32) / 255.0, tf.cast(x_test, tf.float32) / 255.0
   x_train = tf.transpose(x_train, [0, 3, 1, 2])
   x_test = tf.transpose(x_test, [0, 3, 1, 2])
   tf.keras.backend.set_image_data_format('channels_first')
 
-  if args.upstride == 1:
-    import upstride.type2.tf.keras.layers as up
-    framework = up
-    factor = 4
-    print("I'm using upstride")
-  else:
-    framework = tf.keras.layers
-    factor = 1
-
-  model = Model(framework, factor)
+  # prepare a model
+  model = Model(args.upstride == 1)
   model.compile(optimizer=tf.keras.optimizers.Adam(),
                 loss='sparse_categorical_crossentropy',
                 metrics=['accuracy'])
 
+  # g-g-go
   tensorboard_cb = tf.keras.callbacks.TensorBoard(log_dir='/tmp/log', histogram_freq=1, profile_batch=[2, 5], write_graph=False, write_images=False)
-
   model.fit(x_train, y_train,
             epochs=60,
             batch_size=128,
--- tests/python_high_level_tests/train_simple_network_scalars.py
+++ tests/python_high_level_tests/train_simple_network_scalars.py
@@ -7,11 +7,21 @@ import sys
 sys.path.append('../../src/python')
 
 
-def Model(framework, input_shape=(3, 32, 32), nclasses=10):
+def Model(upstride=False, input_shape=(3, 32, 32), nclasses=10):
   inputs = tf.keras.layers.Input(input_shape)
   x = inputs
-  x = framework.Conv2D(4, 3, data_format='channels_first', use_bias=False, padding='SAME', activation=tf.nn.relu, require_input_grad=False)(x)
-  x = framework.Conv2D(4, 3, data_format='channels_first', use_bias=False, padding='SAME', activation=tf.nn.relu)(x)
+
+  if upstride:
+    import upstride.type2.tf.keras.layers as up
+    framework = up
+    print("Using upstride")
+  else:
+    framework = tf.keras.layers
+    print("Using Tensorflow")
+
+  kwargs = {'require_input_grad': False} if upstride else {}
+  x = framework.Conv2D(4, 3, data_format='channels_first', use_bias=False, activation=tf.nn.relu, **kwargs)(x)
+  x = framework.Conv2D(4, 3, data_format='channels_first', use_bias=False, activation=tf.nn.relu)(x)
   x = tf.keras.layers.Flatten()(x)
   x = tf.keras.layers.Dense(nclasses)(x)
   logits = tf.keras.layers.Activation('softmax')(x)
@@ -21,33 +31,32 @@ def Model(framework, input_shape=(3, 32, 32), nclasses=10):
 
 
 def main():
+  gpus = tf.config.experimental.list_physical_devices('GPU')
+  for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
   parser = argparse.ArgumentParser()
   parser.add_argument('--upstride', "-u", type=int, default=1, help='')
   args = parser.parse_args()
+
+  # prepare CIFAR10
   (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
-  x_train, x_test = x_train / 255.0, x_test / 255.0
+  x_train, x_test = tf.cast(x_train, tf.float32) / 255.0, tf.cast(x_test, tf.float32) / 255.0
   x_train = tf.transpose(x_train, [0, 3, 1, 2])
   x_test = tf.transpose(x_test, [0, 3, 1, 2])
   tf.keras.backend.set_image_data_format('channels_first')
 
-  if args.upstride == 1:
-    import upstride.scalar.tf.keras.layers as up
-    framework = up
-    print("I'm using upstride")
-  else:
-    framework = tf.keras.layers
-
-  model = Model(framework)
-  model.compile(optimizer=tf.keras.optimizers.Adam(lr=0.0001),
+  # prepare a model
+  model = Model(args.upstride == 1)
+  model.compile(optimizer=tf.keras.optimizers.Adam(),
                 loss='sparse_categorical_crossentropy',
                 metrics=['accuracy'])
 
+  # g-g-go
   tensorboard_cb = tf.keras.callbacks.TensorBoard(log_dir='/tmp/log', histogram_freq=1, profile_batch=[2, 5], write_graph=False, write_images=False)
-
   model.fit(x_train, y_train,
             epochs=60,
-            batch_size=1000,
-            steps_per_epoch=30,
+            batch_size=128,
             validation_data=(x_test, y_test),
             callbacks=[tensorboard_cb,
                        tf.keras.callbacks.TerminateOnNaN()
