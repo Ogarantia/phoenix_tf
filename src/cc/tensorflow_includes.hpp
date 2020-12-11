4ceca60f60c001a8ce817f4c50f5c7af9168aac5
Youenn Lebras <youenn.lebras@upstride.io>
2020-12-11 08:30:04 +0000

    Merged in feature/PE-268_upgrade_stdc++ (pull request #69)
    * update core pointer
    * [PE-268] Upgrade c++ standard
    
    Approved-by: Philipe Moura

--- src/cc/tensorflow_includes.hpp
+++ src/cc/tensorflow_includes.hpp
@@ -2,11 +2,6 @@
 #define EIGEN_USE_GPU
 #endif
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wignored-attributes"       // disabling tensorflow warnings
-
 #include "tensorflow/core/framework/op.h"
 #include "tensorflow/core/framework/op_kernel.h"
 #include "tensorflow/core/framework/common_shape_fns.h"
-
-#pragma GCC diagnostic pop
